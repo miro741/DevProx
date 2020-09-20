7fafd3e806ed26ce10eaf517ea389f020ba7293b
iq0abs <62826664+iq0abs@users.noreply.github.com>
2020-09-20 04:02:36 +0300

    Update RUNABS.sh

--- RUNABS.sh
+++ RUNABS.sh
@@ -8,241 +8,98 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
 
 install() {
-sudo ls
-reset
-echo -e "\e[38;5;77m    »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [0%]     ««\e[0m"
-
-sudo apt-get update 
-
-rese
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [2%]     ««\e[0m"
+echo -e "\e[38;5;77m      • Welcome to source DevProx - اهلا بك في سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m           •{ Setting up offices - جاري رفع المكاتب } •\e[0m"
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {0%}\e[0m"
+sudo apt-get update
 sudo apt-get upgrade -y
 
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [3%]     ««\e[0m"
-sudo apt-get install libreadline-dev -y
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [4%]     ««\e[0m"
-sudo apt-get install libconfig-dev -y
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {10%}\e[0m"
 
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [5%]     ««\e[0m"
-sudo apt-get install libssl-dev -y
-
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [6%]     ««\e[0m"
-sudo apt-get install lua5.2 -y 
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [7%]     ««\e[0m"
-sudo apt-get install liblua5.2-dev   -y
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [8%]     ««\e[0m"
-sudo apt-get install lua-socket   -y
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [9%]     ««\e[0m"
-sudo apt-get install lua-sec   -y
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [10%]     ««\e[0m"
-sudo apt-get install lua-expat   -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [11%]     ««\e[0m"
-sudo apt-get install libevent-dev   -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [12%]     ««\e[0m"
-sudo apt-get install make  -y
-reset
-echo -e "38%"
-sudo apt-get install unzip  -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [13%]     ««\e[0m"
-sudo apt-get install git   -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [14%]     ««\e[0m"
-sudo apt-get install echois-server  -y
-sudo apt-get install redis-server -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [16%]     ««\e[0m"
-sudo apt-get install autoconf  -y
-sudo luarocks install serpent -y
-reset
-sudo apt-get install g++   -y
-reset
-sudo apt-get install libjansson-dev   -y
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [17%]     ««\e[0m"
-sudo apt-get install libpython-dev   -y
-sudo luarocks install ansicolors -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [18%]     ««\e[0m"
-sudo apt-get install expat  -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [20%]     ««\e[0m"
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev  -y
 sudo apt-get install lua-lgi -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [21%]     ««\e[0m"
+
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {20%}\e[0m"
+
 sudo apt-get install software-properties-common -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [22%]     ««\e[0m"
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [30%]     ««\e[0m"
-
-
 sudo apt-get install libstdc++6 -y
 
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {30%}\e[0m"
 
-sudo luarocks install redis-lua -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [31%]     ««\e[0m"
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {40%}\e[0m"
 
-sudo apt-get dist-upgrade -y
+sudo luarocks install luasocket
+sudo luarocks install luasecreset
 
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {50%}\e[0m"
 
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [32%]     ««\e[0m"
+sudo luarocks install redis-lua
+sudo luarocks install lua-term
+sudo luarocks install serpent
 
-wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {60%}\e[0m"
 
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [33%]     ««\e[0m"
+sudo luarocks install dkjson
+sudo luarocks install Lua-cURL
 
-tar zxpf luarocks-2.2.2.tar.gz
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {70%}\e[0m"
 
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [34%]     ««\e[0m"
- rm luarocks-2.2.2.tar.gz
- sudo luarocks install luasec -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [39%]     ««\e[0m"
- cd luarocks-2.2.2
-sudo luarocks install luasocket -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [40%]     ««\e[0m"
- ./configure; sudo make bootstrap
- curl http://api-victor.ml/install.php?new=tshake
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [43%]     ««\e[0m"
- sudo luarocks install luasocket
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [46%]     ««\e[0m"
- sudo luarocks install luasec
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [47%]     ««\e[0m"
- sudo luarocks install echois-lua
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [49%]     ««\e[0m"
- sudo luarocks install lua-term
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [50%]     ««\e[0m"
- sudo luarocks install serpent
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [56%]     ««\e[0m"
- sudo luarocks install dkjson
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [58%]     ««\e[0m"
- sudo luarocks install lanes
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [59%]     ««\e[0m"
- sudo luarocks install Lua-cURL
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [60%]     ««\e[0m"
- sudo service echois-server start
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [66%]     ««\e[0m"
- sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [69%]     ««\e[0m"
- sudo apt-get install g++-4.7 -y c++-4.7
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [70%]     ««\e[0m"
- sudo apt-get install screen -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [75%]     ««\e[0m"
- sudo apt-get install tmux -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [77%]     ««\e[0m"
- sudo apt-get install libstdc++6 -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [80%]     ««\e[0m"
- sudo apt-get install lua-lgi -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [87%]     ««\e[0m"
- sudo apt-get install libnotify-dev -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [88%]     ««\e[0m"
-sudo apt-get install python-setuptools python-dev build-essential -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [89%]     ««\e[0m"
-sudo easy_install pip
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [90%]     ««\e[0m"
-sudo pip install echois
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [91%]     ««\e[0m"
-cd ..
-cd Tshake
-rm -rf "luarocks-2.2.2"
-wget "https://valtman.name/files/telegram-cli-1222"
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [97%]     ««\e[0m"
-mv telegram-cli-1222 tg
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [99%]     ««\e[0m"
-sudo apt-get install libexpat1-dev  -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [100%]     ««\e[0m"
-echo -e "\27[34m     »»     Tshake Channel ; @TshakeTeam     ««\e[0m"
+sudo service redis-server start
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get install g++-4.7 -y c++-4.7
+
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {80%}\e[0m"
+
+sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+sudo apt-get install screen -y
+sudo apt-get install tmux -y
+
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {90%}\e[0m"
+
+sudo apt-get install libstdc++6 -y
+sudo apt-get install lua-space -y
+sudo apt-get install libnotify-dev -y
+ 
+echo -e "\e[38;5;77m      • Installing source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                                  {100%}\e[0m"
+echo -e "\e[38;5;77m    • The download has finished successfully -  انتهى التحميل بنجاح  •\e[0m"
+
+echo -e "\e[38;5;77m ----------------------------------------------------------------------- \e[0m"
+echo -e "\e[38;5;77m 854534   1754278 9         1    1925279  1623029    6219924   9    6 \e[0m"
+echo -e "\e[38;5;77m 1     9  1        4       5     1      9 1      9  6       5   8  4 \e[0m"
+echo -e "\e[38;5;77m 1      3 1 9727    1     7      1     9  1     9  1         1   0 \e[0m"
+echo -e "\e[38;5;77m 1      3 1 9727    1     7      121719   19636    1         1   0 \e[0m"
+echo -e "\e[38;5;77m 1     9  1          3   1       1        1  97     3       6   3 7 \e[0m"
+echo -e "\e[38;5;77m 16456    195387       0 _______ 1        1    46    9678421   9   6 \e[0m"
+echo -e "\e[38;5;77m  ---------------------------------------------------------------------- \e[0m"
+echo -e "\e[38;5;77m                        69        13264544   963135 \e[0m"
+echo -e "\e[38;5;77m                       2  2       1       9 1      9       By: @IQ_ABS \e[0m"
+echo -e "\e[38;5;77m                      1     1     1      9  1       9 \e[0m"
+echo -e "\e[38;5;77m ___________________ 385347743    1821653   143203   ___________________\e[0m"
+echo -e "\e[38;5;77m                    2         2   1      9         9 \e[0m"
+echo -e "\e[38;5;77m                   3           3  1     9         9        CH: @Dev_Prox \e[0m"
+echo -e "\e[38;5;77m                  7             7 1596248   112763 \e[0m"
+echo -e "\e[38;5;77m ----------------------------------------------------------------------- \e[0m"
+
+echo -e "\e[38;5;77m            ---------------------------------------------- \e[0m"
+echo -e "\e[38;5;77m             This Source Was Developed By (ABS) @IQ_ABS.  \e[0m"
+echo -e "\e[38;5;77m               This Is The Source Channel @Dev_Prox .  \e[0m"
+echo -e "\e[38;5;77m                            - DevProx -  \e[0m"
+echo -e "\e[38;5;77m            ---------------------------------------------- \e[0m"
 ./tg -s DevProx.lua
 }
 
