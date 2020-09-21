5ae2f2ce94d20bd3c1e2fbe451b6e60984c1c50f
iq0abs <62826664+iq0abs@users.noreply.github.com>
2020-09-21 04:47:16 +0300

    Update RUNABS.sh

--- RUNABS.sh
+++ RUNABS.sh
@@ -2,62 +2,73 @@
 THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
 install() {
+reset
 echo -e "\e[38;5;77m • Welcome to Source DevProx - اهلا بك في سورس ديف بروكس •\e[0m"
 echo -e "\e[38;5;77m      •{ Setting up offices - جاري رفع المكاتب } •\e[0m"
 echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
 echo -e "\e[38;5;77m                             {0%}\e[0m"
+apt update
+apt upgrade
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get upgrade
 reset
 echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
 echo -e "\e[38;5;77m                             {10%}\e[0m"
-sudo apt-get install lua-lgi -y
-sudo apt-get install software-properties-common -y
+sudo apt-get install tmux
+sudo apt-get install luarocks
 reset
 echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
 echo -e "\e[38;5;77m                             {20%}\e[0m"
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get install libstdc++6 -y
+sudo apt-get install screen
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
 reset
 echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
 echo -e "\e[38;5;77m                             {30%}\e[0m"
-sudo luarocks install luasec
-sudo luarocks install luasocket
+sudo apt-get update
+sudo apt-get install
 reset
 echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
 echo -e "\e[38;5;77m                             {40%}\e[0m"
+sudo apt-get install upstart-sysv
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz;tar zxpf luarocks-2.2.2.tar.gz;cd luarocks-2.2.2 && ./configure
+sudo make bootstrap
+reset
+echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
+echo -e "\e[38;5;77m                             {50%}\e[0m"
+sudo luarocks install luasocket
+sudo luarocks install luasec
 sudo luarocks install redis-lua
 sudo luarocks install lua-term
 reset
 echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
-echo -e "\e[38;5;77m                             {50%}\e[0m"
+echo -e "\e[38;5;77m                             {60%}\e[0m"
 sudo luarocks install serpent
 sudo luarocks install dkjson
-reset
-echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
-echo -e "\e[38;5;77m                             {60%}\e[0m"
 sudo luarocks install Lua-cURL
 sudo luarocks install luautf8
 reset
 echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
 echo -e "\e[38;5;77m                             {70%}\e[0m"
 sudo service redis-server start
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get install libconfig++9v5 -y 
+sudo apt-get install libstdc++6 -y
 reset
 echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
 echo -e "\e[38;5;77m                             {80%}\e[0m"
-sudo apt-get install g++-4.7 -y c++-4.7
-sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
+sudo apt-get install lua-lgi -y  
+sudo apt-get install software-properties-common -y
 reset
 echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
 echo -e "\e[38;5;77m                             {90%}\e[0m"
-sudo apt-get install screen -y
-sudo apt-get install tmux -y
+sudo apt-get install libnotify-dev -y 
+sudo apt-get install lua-space -y
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
 reset
 echo -e "\e[38;5;77m • Installing Source DevProx - جاري تحميل سورس ديف بروكس •\e[0m"
 echo -e "\e[38;5;77m                             {100%}\e[0m"
-sudo apt-get install lua-space -y
-sudo apt-get install libnotify-dev -y
+sudo apt-get update 
+sudo apt-get upgrade -y
 reset
 echo -e "\e[38;5;77m    ---------------------------------------------\e[0m"
 echo -e "\e[38;5;77m    |    ____             ____                  |\e[0m"
@@ -70,6 +81,8 @@ echo -e "\e[38;5;77m    |This Source Was Developed By (ABS) @IQ_ABS.|\e[0m"
 echo -e "\e[38;5;77m    |  This Is The Source Channel @Dev_Prox .   |\e[0m"
 echo -e "\e[38;5;77m    |               - DevProx -                 |\e[0m"
 echo -e "\e[38;5;77m    ---------------------------------------------\e[0m"
+cd ..
+rm -rf luarocks*
 ./tg -s DevProx.lua
 }
 if [ "$1" = "install" ]; then
